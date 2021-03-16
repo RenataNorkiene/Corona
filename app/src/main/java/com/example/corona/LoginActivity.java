c982564dfc9823063db714af3cdae8e1688d862d
RenataNorkiene <renata.norkiene@gmail.com>
2021-03-16 15:10:12 +0200

    Pridetas perejimas tarp langu.

--- app/src/main/java/com/example/corona/LoginActivity.java
+++ app/src/main/java/com/example/corona/LoginActivity.java
@@ -2,13 +2,45 @@ package com.example.corona;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
-public class LoginActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity { //klases pradzia
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
-    }
-}
+    protected void onCreate(Bundle savedInstanceState) { //funkcijos pradzia
+        super.onCreate(savedInstanceState); // tuscio lango sukurimas
+        setContentView(R.layout.activity_login); //suteik siam langui si vaizda (kodas siejamas su vaizdu)
+
+        EditText username = findViewById(R.id.username);//susiejamas kintamasis vaizde su kintamuoju kode
+        EditText password = findViewById(R.id.password);
+        Button login = findViewById(R.id.login);
+
+        //kodas, susijes su mygtuko paspaudimu
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) { //onClick pradzia
+                // cia rasomas kodas, kuris bus vykdomas kai paspausime mygtuka
+                String usernameStr = username.getText().toString();  // String  vienintelis is didziosios raides
+                String passwordStr = password.getText().toString();
+
+                Toast.makeText(LoginActivity.this,  "Prisijungimo vardas: " +
+                        usernameStr + "\n" + "Slaptazodis: " + passwordStr, Toast.LENGTH_SHORT).show();
+
+
+                Intent goToSearchActivity = new Intent(LoginActivity.this, SearchActivity.class);  //pirmas is kur(this nes esam sitoje klaseje), o antras i kur (class)
+                startActivity(goToSearchActivity);
+
+            } //onclick pabaiga
+        }); //eina kartu jeigu tai yra baigiasi funkcija mygtuko paspaudimu
+
+    } //funkcijos pabaiga
+
+}//klases pabaiga
+
+
---
+++ app/src/main/java/com/example/corona/Validation.java
@@ -0,0 +1,6 @@
+package com.example.corona;
+
+public class Validation {
+    public static final String USERNAME_REGEX_PATTERN = "^[a-z0-9A-Z]{3,20}$";
+
+}
