4ecd8a8bbaa361ecd805bd56c6439c9b6c6b6020
RenataNorkiene <renata.norkiene@gmail.com>
2021-03-20 00:12:18 +0200

    Register

--- app/src/main/java/com/example/corona/LoginActivity.java
+++ app/src/main/java/com/example/corona/LoginActivity.java
@@ -16,40 +16,39 @@ public class LoginActivity extends AppCompatActivity { //klases pradzia
         super.onCreate(savedInstanceState); // tuscio lango sukurimas
         setContentView(R.layout.activity_login); //suteik siam langui si vaizda (kodas siejamas su vaizdu)
 
-        EditText username = findViewById(R.id.username);//susiejamas kintamasis vaizde su kintamuoju kode
+        EditText username = findViewById(R.id.username);
         EditText password = findViewById(R.id.password);
         Button login = findViewById(R.id.login);
-
-        //kodas, susijes su mygtuko paspaudimu
+        Button register = findViewById(R.id.register);
 
         login.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) { //onClick pradzia
-                // cia rasomas kodas, kuris bus vykdomas kai paspausime mygtuka
-                String usernameStr = username.getText().toString();  // String  vienintelis is didziosios raides
+            public void onClick(View v) {
+
+                String usernameStr = username.getText().toString();
                 String passwordStr = password.getText().toString();
 
-                username.setError(null);
-                password.setError(null);//issivalome klaidu zurnala (username ir password)
-
-                if (Validation.isUsernameValid(usernameStr) && Validation.isPasswordValid(passwordStr)) { //jeigu bus validus duomenys pereisim is vieno lango i kita
-                    Intent goToSearchActivity = new Intent(LoginActivity.this, SearchActivity.class);  //pirmas is kur(this nes esam sitoje klaseje), o antras i kur (class)
+                if (Validation.isUsernameValid(usernameStr) && Validation.isUsernameValid(passwordStr)) {
+                    Toast.makeText(LoginActivity.this, "Username: " + usernameStr + "\n" + "Password: " + passwordStr, Toast.LENGTH_LONG).show();
+                    Intent goToSearchActivity = new Intent(LoginActivity.this, SearchActivity.class); //parametrai: iš kur (visad su this, nes šita klasė), į kur (visad su class)
                     startActivity(goToSearchActivity);
-                } else if (!Validation.isUsernameValid(usernameStr)) { //jeigu nebus validus ismesime i ekrana klaida
-                    username.setError(getResources().getString(R.string.login_invalid_username));
-                    username.requestFocus();
-                    // Toast.makeText(LoginActivity.this, getResources().getString(R.string.login_invalid_username),  Toast.LENGTH_SHORT).show();
                 } else {
-                    password.setError(getResources().getString(R.string.login_invalid_password));
-                    password.requestFocus();
+                    username.setError(getResources().getString(R.string.login_invalid_credentials));
+                    username.requestFocus();
+
                 }
+            }
+        });
 
-            } //onclick pabaiga
+        register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
 
-        }); //eina kartu jeigu tai yra baigiasi funkcija mygtuko paspaudimu
-
-    } //funkcijos pabaiga
-
-}//klases pabaiga
+                Intent goToRegisterActivity = new Intent(LoginActivity.this, RegisterActivity.class);
+                startActivity(goToRegisterActivity);
 
 
+            }
+        });
+    }
+}
--- app/src/main/java/com/example/corona/Validation.java
+++ app/src/main/java/com/example/corona/Validation.java
@@ -3,23 +3,29 @@ package com.example.corona;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+
+
 public class Validation {
-    public static final String USERNAME_REGEX_PATTERN = "^[a-z0-9A-Z]{3,20}$"; //globalus kintamasis, matomas visur uz funkcjos ribu
-
+    public static final String USERNAME_REGEX_PATTERN = "^[a-zA-Z0-9]{3,20}$";
     public static final String PASSWORD_REGEX_PATTERN = "^[a-zA-Z0-9.!@_]{5,20}$";
+    public static final String EMAIL_REGEX_PATTERN = "^[a-zA-Z0-9@._-]{10,50}$";
 
-    public static boolean isPasswordValid(String password){
-        Pattern pattern = Pattern.compile(PASSWORD_REGEX_PATTERN);
-        Matcher matcher = pattern.matcher(password);
-        boolean isPasswordValid = matcher.find();
-        return isPasswordValid;
+    public static boolean isUsernameValid(String username) {
+        return isCredentialsValid(username, USERNAME_REGEX_PATTERN);
     }
-
-    public static boolean isUsernameValid(String username){
-        Pattern pattern = Pattern.compile(USERNAME_REGEX_PATTERN); //sukuriamos username validacijai taisykles pagal nurodyta sablona
-        Matcher matcher = pattern.matcher(username); //pagal anksciau sukurtas taisykles palyginimi vartotojo ivesti duomenys(username)
-        boolean isUsernameValid = matcher.find(); //jeigu atitinka - grazins true, kitu atveju - false
-        return isUsernameValid;
-    }   // true (1) arba false (0)
-
+    public static boolean isPasswordValid(String password) {
+        return isCredentialsValid(password, PASSWORD_REGEX_PATTERN);
+    }
+    public static boolean isEmailValid(String email) {
+        return isCredentialsValid(email, EMAIL_REGEX_PATTERN);
+    }
+    private static boolean isCredentialsValid(String string, String regexPattern) {
+        Pattern pattern = Pattern.compile(regexPattern); //sukuriamos taisyklės, pagal kurias vyks validacija username
+        Matcher matcher = pattern.matcher(string);  //matcher yra svarstyklės,
+        return matcher.find();  //jei tinka, grąžins true
+    }
 }
+
+
+
+
