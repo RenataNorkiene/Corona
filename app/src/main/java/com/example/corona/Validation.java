0410216dd569a1f38ea144a7622aa3455731dec9
RenataNorkiene <renata.norkiene@gmail.com>
2021-03-23 15:44:13 +0200

    Register

--- app/src/main/java/com/example/corona/Validation.java
+++ app/src/main/java/com/example/corona/Validation.java
@@ -8,7 +8,9 @@ import java.util.regex.Pattern;
 public class Validation {
     public static final String USERNAME_REGEX_PATTERN = "^[a-zA-Z0-9]{3,20}$";
     public static final String PASSWORD_REGEX_PATTERN = "^[a-zA-Z0-9.!@_]{5,20}$";
-    public static final String EMAIL_REGEX_PATTERN = "^[a-zA-Z0-9@._-]{10,50}$";
+    public static final String EMAIL_REGEX_PATTERN = "^[A-Za-z0-9+_.-]+@(.+){10,50}$";
+    //"^[a-zA-Z0-9@._-]{10,50}$";
+            ;
 
     public static boolean isUsernameValid(String username) {
         return isCredentialsValid(username, USERNAME_REGEX_PATTERN);
