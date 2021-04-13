31628cd9482756f95619b479fb2105bdf83665bf
RenataNorkiene <renata.norkiene@gmail.com>
2021-04-13 15:36:08 +0300

    Getting data from API

--- app/src/main/java/com/example/corona/JSON.java
+++ app/src/main/java/com/example/corona/JSON.java
@@ -7,7 +7,9 @@ import java.io.InputStreamReader;
 import java.io.Reader;
 import java.net.URL;
 import java.nio.charset.Charset;
+import java.util.ArrayList;
 
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
@@ -20,7 +22,7 @@ public class JSON {
             sb.append((char) cp);
         }
         return sb.toString();
-    }
+    } //readAll end
 
     public static JSONObject readJsonFromUrl(String url) throws IOException, JSONException {
         InputStream is = new URL(url).openStream();
@@ -32,6 +34,49 @@ public class JSON {
         } finally {
             is.close();
         }
+    }//readJsonFromUrl end
+// trys metodai svarbus norint issitraukti norimus duomenis is JSON
+    public static ArrayList<Corona> getList(JSONArray jsonArray) throws JSONException{
+        ArrayList<Corona> coronaList = new ArrayList<Corona>(); //created list with all counties
+        //extract data from JSON and save it in Corona Objects List(coronaList)
+        for (int i=0; i<jsonArray.length(); i++){
+            JSONObject jsonObject = jsonArray.getJSONObject(i);
+            Corona corona = new Corona( //constructor for one object
+                    // public Corona(String country, String lastUpdate, String keyId, int confirmed, int deaths)
+                    jsonObject.getString("country"),
+                    jsonObject.getString("lastUpdate"),
+                    jsonObject.getString("keyId"),
+                    jsonObject.getInt("confirmed"),
+                    jsonObject.getInt("deaths")
+            );
+            coronaList.add(corona); //every objects of JSON goes true cycle and extracts data (country, lastUpdate etc.)
+        }
+
+
+        return coronaList;
+    }
+    public static JSONArray getJSONArray(JSONObject jsonObject) throws JSONException{ //from array extract list
+       //remove from JSON all info, which is not needed, exept covid19Stats Array
+        int jsonLength = jsonObject.toString().length();
+        String covid19Stats = "{" + jsonObject.toString().substring(96, jsonLength) + "}"; //substring delete some symbols from string (starts from 96 till the end)
+
+        //string convert to JSONObject
+        JSONObject jsonObject1 = new JSONObject(covid19Stats);
+        //JSONObject convert to JSONArray
+        JSONArray jsonArray = jsonObject1.getJSONArray("covid19Stats");
+        return jsonArray; //sita jsonArray paims aukstesnis metodas getList ir grazins mums sarasa
     }
 
-}
+    public static ArrayList<Corona> getCoronaListByCountry(ArrayList<Corona> coronaArrayList, String country){
+        ArrayList<Corona> coronaListByCountry = new ArrayList<Corona>();
+        for (Corona corona : coronaArrayList) { //kaireje bus sukuriamas tos klases objektas, per kurios sarasa iteruojame (desineje)
+            if(corona.getKeyId().contains(country)){ //contains method (string method)  search part of string/word (country)
+                coronaListByCountry.add(corona);
+            }
+        }
+        return coronaListByCountry;
+    }
+
+
+
+}//json end
--- app/src/main/java/com/example/corona/SearchActivity.java
+++ app/src/main/java/com/example/corona/SearchActivity.java
@@ -7,14 +7,18 @@ import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.io.IOException;
 import java.net.HttpURLConnection;
+import java.util.ArrayList;
 
 public class SearchActivity extends AppCompatActivity {
     public static final String COVID_API = "https://covid19-api.weedmark.systems/api/v1/stats";
+    private ArrayList<Corona> coronaList = new ArrayList<Corona>();
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,7 +27,7 @@ public class SearchActivity extends AppCompatActivity {
 
         //new thread(gija) starts (to read JSON from API)
         AsyncFetch asyncFetch = new AsyncFetch();
-        asyncFetch.execute();
+        asyncFetch.execute();//must be called to start onPressExecute, doInBackground
     }
 
 
@@ -41,9 +45,9 @@ public class SearchActivity extends AppCompatActivity {
         @Override
         protected JSONObject doInBackground(String... strings) { //will be executed while user waits (see progressDialog), gets JSON from API
             try {
-                JSONObject jsonObject = JSON.readJsonFromUrl(COVID_API);
+                JSONObject jsonObject = JSON.readJsonFromUrl(COVID_API); //sioje vietoje perduosime URL
                 return jsonObject;
-            } catch (IOException e) {
+            } catch (IOException e) { //input/output exeptions
                 Toast.makeText(
                         SearchActivity.this,
                         getResources().getString(R.string.search_error_reading_data) + e.getMessage(),
@@ -63,7 +67,7 @@ public class SearchActivity extends AppCompatActivity {
         protected void onPostExecute(JSONObject jsonObject) {
             progressDialog.dismiss();
 
-            int statusCode = 0;
+            int statusCode = 0; //kol kas jo dar nezinome, todel 0
             try {
                 statusCode = jsonObject.getInt("statusCode");
             } catch (JSONException e) {
@@ -73,13 +77,19 @@ public class SearchActivity extends AppCompatActivity {
                         Toast.LENGTH_LONG
                 ).show();
             }//catch ends
-            if(statusCode == HttpURLConnection.HTTP_OK) {
-                System.err.println(jsonObject.toString());
-                Toast.makeText(SearchActivity.this, jsonObject.toString(),
-                        Toast.LENGTH_LONG
-                ).show();
+            if (statusCode == HttpURLConnection.HTTP_OK) {
+                //system.err.println(jsonObject.toString());
+                JSONArray jsonArray = null;
+                try {
+                    jsonArray = JSON.getJSONArray(jsonObject);
+                    coronaList = JSON.getList(jsonArray);
+                    System.out.println("Lithuania covidStats:" + JSON.getCoronaListByCountry(coronaList, "Italy"));
+                } catch (JSONException e) {
+                    System.out.println(getResources().getString(R.string.search_error_reading_data) + e.getMessage());
+                }
+//
             } else { //if something is wrong, not ok(200 code)
-                String message = null;
+                String message = null; //susikuriame nes nezinome kokia bus zinute
                 try {
                     message = jsonObject.getString("message");
                 } catch (JSONException e) {
@@ -91,7 +101,7 @@ public class SearchActivity extends AppCompatActivity {
                 } //catch ends
                 Toast.makeText(
                         SearchActivity.this,
-                        getResources().getString(R.string.search_error_reading_data) + message,
+                        getResources().getString(R.string.search_error_reading_data) + message, //message = kokia zinute ateina is API
                         Toast.LENGTH_LONG
                 ).show();
             } //else ends
